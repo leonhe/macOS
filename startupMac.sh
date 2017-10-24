94ea08915daa7251614fe24f9a25c39aeab64740
Yuanfei <hi@leonhe.me>
2017-10-24 10:57:57 +0800

    add startup tools

---
+++ startupMac.sh
@@ -0,0 +1,10 @@
+#!/usr/bin
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#zsh
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install emacs #emacs
+#Download tools
+brew install aria2
+
+
