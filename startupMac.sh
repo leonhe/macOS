8dac08e301978d74063de68f01ac59f667f09fe1
Leon He <lhe868@me.com>
2019-06-18 10:55:07 +0800

    add nodejs script

--- startupMac.sh
+++ startupMac.sh
@@ -1,12 +1,27 @@
 #!/usr/bin
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#replace brew repo site url
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+brew update
+
 #zsh
 brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 brew install emacs #emacs
 #Download tools
 brew install aria2
 #Program tools
 brew install global
+#nodejs
+brew install node
+brew install npm
+#nodejs project debug tool
+npm install -g indium
 
 
